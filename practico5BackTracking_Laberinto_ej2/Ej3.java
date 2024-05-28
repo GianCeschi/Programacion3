40477e8761fa19b68149f3e4a55dd69e018738ef
Pablo Andersen <90287403+pablo-andersen@users.noreply.github.com>
2024-05-28 20:11:39 -0300

    Ej3 actualizacion

--- practico5BackTracking_Laberinto_ej2/Ej3.java
+++ practico5BackTracking_Laberinto_ej2/Ej3.java
@@ -1,29 +1,30 @@
 package practico5BackTracking_Laberinto_ej2;
 
+import java.util.LinkedList;
+
 public class Ej3 {
-    
+    LinkedList<LinkedList<Integer>> soluciones = new LinkedList<LinkedList<Integer>>();
+    Integer m;
+    public LinkedList<LinkedList<Integer>> subconjuntosIgualM (LinkedList<Integer> conjunto, Integer m){
+        this.m = m;    
+        LinkedList<Integer> resParcial = new LinkedList<Integer>();
+        backtracking(conjunto, resParcial);
+        return soluciones;
+    }
+    // c'= {1,1,0,0}
+    private void backtracking(LinkedList<Integer> conjunto, LinkedList<Integer> resParcial){
+        if(conjunto.isEmpty()){                             //ESTADO FINAL
+            if(esSolucion(conjunto, resParcial)){                     //devuelve si la suma de los elementos del conjunto es igual a m.
 
-    conjunto C = {4,1,5,6}
-M = 5
+            }
+        }
+    }
 
-c'= {1,1,0,0}
-
-list<list <int>> soluciones;
-int M ;
-
-
-public list<int> subconjuntosIgualM (Subconjunto list C, Int M){
-   this.M = M;
-   list<Int> resParcial = new list..;
-   backtracking(C,resParcial);
-   return soluciones;
-}
-
-
-private void backtracking(list C, list resParcial){
-  if(c.isEmpty()){ //ESTADO FINAL
-     if()
-  }
-
-}
+    private boolean esSolucion(LinkedList<Integer> conjunto, LinkedList<Integer> resParcial){
+        Integer suma = 0;
+        for(Integer i = 0; i < resParcial.size(); i++){
+            suma += i;
+        }
+        return suma == m;
+    }
 }
