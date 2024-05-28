edaac2d9346401c070e0daa57d3000b0c8e063d3
Pablo Andersen <90287403+pablo-andersen@users.noreply.github.com>
2024-05-28 19:51:38 -0300

    agrego ej3

---
+++ practico5BackTracking_Laberinto_ej2/Ej3.java
@@ -0,0 +1,29 @@
+package practico5BackTracking_Laberinto_ej2;
+
+public class Ej3 {
+    
+
+    conjunto C = {4,1,5,6}
+M = 5
+
+c'= {1,1,0,0}
+
+list<list <int>> soluciones;
+int M ;
+
+
+public list<int> subconjuntosIgualM (Subconjunto list C, Int M){
+   this.M = M;
+   list<Int> resParcial = new list..;
+   backtracking(C,resParcial);
+   return soluciones;
+}
+
+
+private void backtracking(list C, list resParcial){
+  if(c.isEmpty()){ //ESTADO FINAL
+     if()
+  }
+
+}
+}
