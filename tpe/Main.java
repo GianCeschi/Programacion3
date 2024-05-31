79e9760d8f1fe84a9468fb2218a46f3ca7feb6c3
Pablo Andersen <90287403+pablo-andersen@users.noreply.github.com>
2024-05-30 23:18:37 -0300

    actualizo path tareas y procesadores

--- tpe/Main.java
+++ tpe/Main.java
@@ -3,7 +3,7 @@ package tpe;
 public class Main {
 
 	public static void main(String args[]) {
-		Servicios servicios = new Servicios("./Programacion3/tpe/datasets/Procesadores.csv", "./Programacion3/tpe/datasets/Tareas.csv");
+		Servicios servicios = new Servicios("./src/tpe/datasets/Procesadores.csv", "./src/tpe/datasets/Tareas.csv");
 
 		// servicios.servicio1("T1");
 		// servicios.servicio2(true);
